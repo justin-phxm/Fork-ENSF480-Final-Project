56cccc4ff39f832c1914bff2dd77dc6200ac85f5
freshmint7 <joseph.duong@ucalgary>
2023-11-16 15:12:03 -0700

    react and spring initialized

---
+++ group12/src/main/java/ensf480/ucalgary/group12/Group12Application.java
@@ -0,0 +1,13 @@
+package ensf480.ucalgary.group12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group12Application.class, args);
+	}
+
+}
---
+++ group12/src/test/java/ensf480/ucalgary/group12/Group12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ensf480.ucalgary.group12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

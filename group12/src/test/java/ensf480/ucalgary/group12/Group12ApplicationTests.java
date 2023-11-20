fba6fbae98b476db2abbede3db1aab90b1ef1090
freshmint7 <djosephbuisness@gmail.com>
2023-11-19 22:20:10 -0700

    spring boots

--- group12/src/test/java/ensf480/ucalgary/group12/Group12ApplicationTests.java
+++ group12/src/test/java/ensf480/ucalgary/group12/Group12ApplicationTests.java
@@ -3,7 +3,7 @@ package ensf480.ucalgary.group12;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Group12Application.class)
 class Group12ApplicationTests {
 
 	@Test

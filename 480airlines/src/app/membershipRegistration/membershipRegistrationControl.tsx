b7fd204e2ea082c1d3d0a6bc6011c6e5b1daaae9
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-11-20 23:25:52 -0700

    Add member signup form

--- 480airlines/src/app/membershipRegistration/membershipRegistrationControl.tsx
+++ 480airlines/src/app/membershipRegistration/membershipRegistrationControl.tsx
@@ -2,6 +2,55 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-export default function membershipRegistrationControl() {
-  return <div>membershipRegistrationControl</div>;
+import { useSession } from "next-auth/react";
+export default function MembershipRegistrationControl() {
+  const { data: session, status } = useSession();
+  const handleOnSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (session) {
+      console.log(session.user?.email);
+      toast.success("You have successfully signed up for membership!");
+    }
+  };
+  return (
+    <>
+      <form className="w-full max-w-md mx-auto">
+        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+          Email sign-up
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 16"
+            >
+              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          </div>
+          <input
+            type="text"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder={
+              session
+                ? session.user?.email || "Please make sure you are logged in"
+                : `Please make sure you are logged in`
+            }
+            disabled
+          />
+          <button
+            type="submit"
+            onClick={handleOnSubmit}
+            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+      <ToastContainer position="bottom-right"></ToastContainer>
+    </>
+  );
 }

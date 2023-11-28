5f9fd643fc53f02284a8581e14c0a8bb4f6bc002
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-11-28 10:34:47 -0700

    split membership into components

---
+++ 480airlines/src/app/membershipRegistration/components/MembershipHero.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import MembershipRegistrationControl from "../membershipRegistrationControl";
+
+export default function MembershipHero() {
+  return (
+    <section className="relative bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
+        <a
+          href="#"
+          className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800"
+        >
+          <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
+            New
+          </span>
+          <span className="text-sm font-medium">
+            Exclusive monthly promotions! See whats new
+          </span>
+          <svg
+            className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+        </a>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Our Members Get The Best Benefits
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
+          Here at 480Airlines we believe in giving our members the best
+          benefits. Sign up now for free!
+        </p>
+        <MembershipRegistrationControl></MembershipRegistrationControl>
+      </div>
+      <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+    </section>
+  );
+}
--- 480airlines/src/app/membershipRegistration/page.tsx
+++ 480airlines/src/app/membershipRegistration/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import MembershipRegistrationControl from "./membershipRegistrationControl";
+import MembershipFeatureCards from "./components/membershipFeatureCards";
+import MembershipHero from "./components/MembershipHero";
 
 export default function page() {
   const memberBenefits = [
@@ -32,45 +34,7 @@ export default function page() {
   return (
     <div>
       {/* Hero */}
-      <section className="relative bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
-          <a
-            href="#"
-            className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800"
-          >
-            <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
-              New
-            </span>
-            <span className="text-sm font-medium">
-              Exclusive monthly promotions! See whats new
-            </span>
-            <svg
-              className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-          </a>
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Our Members Get The Best Benefits
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
-            Here at 480Airlines we believe in giving our members the best
-            benefits. Sign up now for free!
-          </p>
-          <MembershipRegistrationControl></MembershipRegistrationControl>
-        </div>
-        <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
-      </section>
+      <MembershipHero></MembershipHero>
 
       {/* Benefits */}
       <section className="bg-white dark:bg-gray-900">
@@ -125,53 +89,11 @@ export default function page() {
           {/* Smaller cards */}
           <ul className="grid md:grid-cols-2 gap-8">
             {memberBenefits.map((benefit, index) => (
-              <li
+              <MembershipFeatureCards
+                benefit={benefit}
+                index={index}
                 key={index}
-                className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12"
-              >
-                <a
-                  href="#"
-                  className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2"
-                >
-                  <svg
-                    className="w-2.5 h-2.5 me-1.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
-                  </svg>
-                  Members Only
-                </a>
-                <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
-                  {benefit.title}
-                </h2>
-                <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
-                  {benefit.body}
-                </p>
-                <a
-                  href="#"
-                  className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center"
-                >
-                  Signup Now!
-                  <svg
-                    className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </li>
+              />
             ))}
           </ul>
         </div>

87ef6b6440a80c84f8487b13c906a5f35c5712ae
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-11-28 14:06:58 -0700

    fetch data from backend

--- 480airlines/src/app/components/DestinationTable.tsx
+++ 480airlines/src/app/components/DestinationTable.tsx
@@ -1,12 +1,21 @@
 "use client";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import DestinationCard from "./DestinationCard";
 import { AppStateContext } from "./FlightContext";
 export default function DestinationTable() {
   const flightProvider = useContext(AppStateContext);
+  const { appState, setAppState } = flightProvider!;
   const flightArray = flightProvider?.appState.flights;
+
+  useEffect(() => {
+    fetch("/api/flight")
+      .then((res) => res.json())
+      .then((data) => {
+        setAppState({ flights: data });
+      });
+  }, []);
   return (
-    <ul className="grid grid-cols-3 gap-4">
+    <ul data-aos={"fade-right"} className="grid grid-cols-3 gap-4">
       {flightArray?.map((flight, index) => (
         <DestinationCard flight={flight} key={index} />
       ))}

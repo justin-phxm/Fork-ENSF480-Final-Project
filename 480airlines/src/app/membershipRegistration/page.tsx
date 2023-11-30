839666559895815cef8aaeb1f8425c28683173b4
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-11-30 11:05:23 -0700

    update membership feature card

--- 480airlines/src/app/membershipRegistration/page.tsx
+++ 480airlines/src/app/membershipRegistration/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MembershipFeatureCards from "./components/MembershipFeatureCards";
 import MembershipHero from "./components/MembershipHero";
+import MembershipFeatureCardLarge from "./components/MembershipFeatureCardLarge";
 
 export default function page() {
   const memberBenefits = [
@@ -39,52 +40,7 @@ export default function page() {
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
           {/* Large Card */}
-          <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <a
-              href="#"
-              className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2"
-            >
-              <svg
-                className="w-2.5 h-2.5 me-1.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 14"
-              >
-                <path d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z" />
-              </svg>
-              For Members
-            </a>
-            <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">
-              Exclusive Monthly Promotions
-            </h1>
-            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
-              Sign up now to be among the first to receive our exciting monthly
-              promotions! Uncover exclusive deals and discounts available only
-              to our registered members.
-            </p>
-            <a
-              href="#"
-              className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-            >
-              Read more
-              <svg
-                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
+          <MembershipFeatureCardLarge></MembershipFeatureCardLarge>
           {/* Smaller cards */}
           <ul className="grid md:grid-cols-2 gap-8">
             {memberBenefits.map((benefit, index) => (
@@ -101,6 +57,7 @@ export default function page() {
             <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
               Pack Your Bags!
             </h2>
+            {/* Image */}
             <svg
               width="374"
               height="449"
